2948d0e397ef8b5b25cbdf39fa7bae3869480123
Marek Sterzik <marek@milimetr.org>
2021-04-09 09:03:06 +0200

    asset version management

---
+++ src/Framework/VersionStrategy.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Framework;
+
+use Symfony\Component\Asset\VersionStrategy\VersionStrategyInterface;
+
+class VersionStrategy implements VersionStrategyInterface
+{
+    /** @var string|null */
+    private $version;
+
+    public function __construct(string $assetBaseDir, string $versionLinkWildCard)
+    {
+        $this->version = $this->detectVersion($assetBaseDir, $versionLinkWildCard);
+    }
+
+    public function getVersion(string $path)
+    {
+        return $this->version;
+    }
+
+    public function applyVersion(string $path)
+    {
+        if ($this->version !== null) {
+            return sprintf("%s/%s", $this->version, $path);
+        } else {
+            return $path;
+        }
+    }
+
+    private function detectVersion(string $assetBaseDir, string $versionWildCard): ?string
+    {
+        $cwd = getcwd();
+        chdir($assetBaseDir);
+        $versions = [];
+        foreach (glob($versionWildCard) as $version) {
+            if (is_dir($version)) {
+                $versions[] = $version;
+            }
+        }
+        chdir($cwd);
+
+        if (empty($versions)) {
+            return null;
+        }
+
+        sort($versions);
+
+        return array_pop($versions);
+    }
+}
