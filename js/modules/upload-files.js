eb55fd45946e069c82212bf62c58e8036f3672cb
Marek Sterzik <marek.sterzik@gmail.com>
2025-05-12 11:14:45 +0200

    work in progress: file renaming UI in the upload files dialog

--- js/modules/upload-files.js
+++ js/modules/upload-files.js
@@ -41,6 +41,51 @@ const enableScrollFeature = (scroll, left, right) => {
     setScrollStatus(scroll, left, right)
 }
 
+const doSelect = (input, from, to) => {
+	input = input[0];
+	if (input.createTextRange) {
+		var range = input.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', to);
+		range.moveStart('character', from);
+		range.select();
+	} else if (input.setSelectionRange) {
+		input.focus();
+		input.setSelectionRange(from, to);
+	}
+}
+
+const createFileSelection = (input) => {
+    const filename = input.val()
+    const match = filename.match(/(\.[a-zA-Z0-9]{1,6})+$/)
+    const len = filename.length - (match ? match[0].length : 0)
+    doSelect(input, 0, len)
+}
+
+const setupEdit = (cell, enabled) => {
+    const showWidget = cell.find(".submitted-file-show")
+    const editWidget = cell.find(".submitted-file-edit")
+    const table = cell.selectParent("table")
+    const prevEnabled = !editWidget.is(":hidden")
+    if (enabled === "toggle") {
+        enabled = !prevEnabled
+    }
+    if (enabled) {
+        showWidget.hide()
+        editWidget.show()
+        if (!prevEnabled) {
+            const inputWidget = editWidget.find("input")
+            inputWidget.focus()
+            createFileSelection(inputWidget)
+        }
+        table.find(".submitted-file-actions-button").addClass("disabled")
+    } else {
+        showWidget.show()
+        editWidget.hide()
+        table.find(".submitted-file-actions-button").removeClass("disabled")
+    }
+}
+
 $(() => {
     $(".submitted-file-item").each(function () {
         const element = $(this)
@@ -48,5 +93,19 @@ $(() => {
         const leftArrow = element.find(".submitted-file-left")
         const rightArrow = element.find(".submitted-file-right")
         enableScrollFeature(scrollElement, leftArrow, rightArrow)
+    })
+    $(".submitted-file-do-edit").bind("click", function (ev) {
+        setupEdit($(this).selectParent("tr").find("td.submitted-file-item"), true)
+        $(this).selectParent("div.btn-group").find(".show").removeClass("show")
+        ev.preventDefault()
+        return false
+    })
+
+    $(".submitted-file-edit input").bind("keydown", function (ev) {
+        if (ev.key == "Escape") {
+            setupEdit($(this).selectParent("tr").find("td.submitted-file-item"), false)
+            ev.preventDefault()
+            return false
+        }
     })
 })
