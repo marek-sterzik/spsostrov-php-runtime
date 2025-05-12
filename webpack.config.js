8a79d643626bea2b2d7377b18638186d943c84d5
Marek Sterzik <marek.sterzik@gmail.com>
2025-05-12 14:19:32 +0200

    add ts/jsx/tsx compiler support to the javascript engine

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
+const path = require('path');
+
 module.exports = {
-  entry: './js/index.js',
+  entry: path.resolve(__dirname, 'js/index.js'),
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   output: {
-    path: __dirname + '/public/js'
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
   },
   module: {
     rules: [
-        {test: /\.js$/, use: "babel-loader"}
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          presets: [
+            require.resolve('@babel/preset-env'),
+            require.resolve('@babel/preset-react'),
+            require.resolve('@babel/preset-typescript')
+          ]
+        }
+      }
     ]
   }
 };
