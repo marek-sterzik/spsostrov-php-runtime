af55284845bb0ccf88f82eabec9051a49afb3556
Marek Sterzik <marek.sterzik@gmail.com>
2025-05-13 17:58:09 +0200

    iframe improvement

--- js/modules/adminer-frame.js
+++ js/modules/adminer-frame.js
@@ -1,5 +1,7 @@
 import $ from "jquery"
 
+var frame
+
 const adjustSize = () => {
     const element = $("div.adminer-frame")
     const wh = $(window).height()
@@ -9,7 +11,28 @@ const adjustSize = () => {
 
 }
 
+const getInitialSrc = (path) => {
+    const hash = window.location.hash.replace(/^#/, '')
+    return path + (hash !== "" ? "?" : "") + hash
+}
+
+const buildHash = (frameLocation, mainLocation, adminerPath) => {
+    if (frameLocation.origin !== mainLocation.origin) {
+        return '';
+    }
+    if (frameLocation.pathname !== adminerPath) {
+        return '';
+    }
+    return frameLocation.search.replace(/^\?/, '')
+}
+
 $(() => {
     adjustSize()
     $(window).resize(adjustSize)
+    frame = $("div.adminer-frame iframe")
+    frame.bind("load", function(){
+        const hash = buildHash(this.contentWindow.location, window.location, $(this).attr("data-src"))
+        window.location.hash = hash
+    })
+    frame.attr("src", getInitialSrc(frame.attr("data-src")))
 })
